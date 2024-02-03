81db2cfe9fb81c219da9cea2590d2abdf20fe8e0
Gabriel Corradi <gabrielcorradi250@gmail.com>
2024-02-03 17:41:21 -0300

    Salvamento do trabalho com 'base-apparel-coming-soon-master/' e refinamento de projetos anteriores

--- faq-accordion-main/script/index.js
+++ faq-accordion-main/script/index.js
@@ -2,11 +2,14 @@ defaulDetail()
 
 function defaulDetail() {
   document.getElementsByClassName('details')[0].setAttribute("open", "")
+  document.getElementsByClassName('svg')[0].setAttribute("src", "assets/images/icon-minus.svg")
 }
 
-function closeFunction() {
+function closeFunction(n) {
   let i;
   for (i = 0;i < 4; i++) {
     document.getElementsByClassName('details')[i].removeAttribute("open")
+    document.getElementsByClassName('svg')[i].setAttribute("src", "assets/images/icon-plus.svg")
   }
+  document.getElementsByClassName('svg')[n-1].setAttribute("src", "assets/images/icon-minus.svg")
 }
